9a2719c27ed501ca26f88d0d05138f811eee8d6f
Shiomar Salazar <s.salazarc@uniandes.edu.co>
2023-09-22 22:08:20 -0600

    Avance de pantallas

--- app/src/main/java/com/alarmaxpro/myapplication/FirstFragment.kt
+++ app/src/main/java/com/alarmaxpro/myapplication/FirstFragment.kt
@@ -33,7 +33,7 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnEntrar.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            findNavController().navigate(R.id.action_FirstFragment_to_mainMenu)
         }
     }
 
---
+++ app/src/main/java/com/alarmaxpro/myapplication/mainMenu.kt
@@ -0,0 +1,55 @@
+package com.alarmaxpro.myapplication
+
+import android.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.alarmaxpro.myapplication.databinding.FragmentMainMenuBinding
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [mainMenu.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class mainMenu : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentMainMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentMainMenuBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
